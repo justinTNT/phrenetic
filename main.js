f3b66216fbbbc72b180516e4c2c0ee1b22551b84
Krzysztof Baranowski <pharcosyle@gmail.com>
2013-03-15 14:19:44 -0400

    ocd

--- main.js
+++ main.js
@@ -6,9 +6,10 @@ var projectRoot = require('path').dirname(module.parent.filename);
 require('./lib/server/config')(projectRoot);
 
 var module = process.argv[2];
+// Special case because the server module takes an arguement.
 if (module == 'server')
 	require('./lib/server')(projectRoot);
-try {
+else try {
 	require(projectRoot + '/lib/' + module);
 } catch (err) {
 	if (err.code != 'MODULE_NOT_FOUND')
